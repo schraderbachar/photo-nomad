fb7b0cbd4c68b7253be032e0141a56744c8b2860
schraderbachar <jj.schraderbachar@icloud.com>
2025-07-13 16:58:14 -0500

    intial commit w input

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.photonomad',
+  appName: 'photo-nomad',
+  webDir: 'build'
+};
+
+export default config;
